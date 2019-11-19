3356bf5a54a80585659d771acd7f5a07a09a3130
selcuk-atlassian <skiraz@atlassian.com>
2019-11-19 11:09:58 +0300

    add "create" command

---
+++ packages/cli/bin/packit.js
@@ -0,0 +1,105 @@
+#!/usr/bin/env node
+
+const chalk = require('chalk');
+const semver = require('semver');
+const requiredVersion = require('../package.json').engines.node;
+const didYouMean = require('didyoumean');
+
+// Setting edit distance to 60% of the input string's length
+didYouMean.threshold = 0.6;
+
+function checkNodeVersion(wanted, id) {
+  if (!semver.satisfies(process.version, wanted)) {
+    console.log(
+      chalk.red(
+        'You are using Node ' +
+          process.version +
+          ', but this version of ' +
+          id +
+          ' requires Node ' +
+          wanted +
+          '.\nPlease upgrade your Node version.'
+      )
+    );
+    process.exit(1);
+  }
+}
+
+checkNodeVersion(requiredVersion, '@vue/cli');
+
+if (semver.satisfies(process.version, '9.x')) {
+  console.log(
+    chalk.red(
+      `You are using Node ${process.version}.\n` +
+        `Node.js 9.x has already reached end-of-life and will not be supported in future major releases.\n` +
+        `It's strongly recommended to use an active LTS version instead.`
+    )
+  );
+}
+
+const program = require('commander');
+
+program
+  .version(`@packit/cli ${require('../package').version}`)
+  .usage('<command> [options]');
+
+program
+  .command('create <app-name>')
+  .description('create a new project')
+  .action((name, cmd) => {
+    const options = cleanArgs(cmd);
+    console.log(chalk.yellow(`Merhaba ${options}`));
+  });
+
+program.arguments('<command>').action(cmd => {
+  program.outputHelp();
+  console.log(`  ` + chalk.red(`Unknown command ${chalk.yellow(cmd)}.`));
+  console.log();
+  suggestCommands(cmd);
+});
+
+program.on('--help', () => {
+  console.log();
+  console.log(
+    `  Run ${chalk.cyan(
+      `packit <command> --help`
+    )} for detailed usage of given command.`
+  );
+  console.log();
+});
+
+program.commands.forEach(c => c.on('--help', () => console.log()));
+
+program.parse(process.argv);
+
+if (!process.argv.slice(2).length) {
+  program.outputHelp();
+}
+
+function suggestCommands(unknownCommand) {
+  const availableCommands = program.commands.map(cmd => {
+    return cmd._name;
+  });
+
+  const suggestion = didYouMean(unknownCommand, availableCommands);
+  if (suggestion) {
+    console.log(`  ` + chalk.red(`Did you mean ${chalk.yellow(suggestion)}?`));
+  }
+}
+
+function camelize(str) {
+  return str.replace(/-(\w)/g, (_, c) => (c ? c.toUpperCase() : ''));
+}
+
+function cleanArgs(cmd) {
+  const args = {};
+  cmd.options.forEach(o => {
+    const key = camelize(o.long.replace(/^--/, ''));
+    // if an option is not present and Command has a method with the same name
+    // it should not be copied
+    if (typeof cmd[key] !== 'function' && typeof cmd[key] !== 'undefined') {
+      args[key] = cmd[key];
+    }
+  });
+  return args;
+}
